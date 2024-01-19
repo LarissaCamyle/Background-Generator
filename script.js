6a2c69706385c4e4d189fea688644013ab46b791
Larissa Camyle <153947517+LarissaCamyle@users.noreply.github.com>
2024-01-19 17:24:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ var color1= document.querySelector(".color1");
 var color2= document.querySelector(".color2");
 var body= document.getElementById("gradient");
 
+
 function background (){
     body.style.background = "linear-gradient(to right, "+ color1.value +", "+ color2.value+ ")";
-
+    console.log("background:" + body.style.background + ";");
 
 }
 
